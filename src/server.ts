9d790ce42dca6f1e2b48eca87edc5e0be5e091e3
sudjoao <isudjoao@gmail.com>
2020-06-17 23:24:42 -0300

    1 - Create backend  base

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+    response.json([
+        'Hello World 2',
+    ]);
+});
+
+app.listen(3333);
